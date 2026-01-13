1c1c861630cbd5e42138c30cb7198ac2c8edd089
Rotmistrz <Rotmistrz@users.noreply.github.com>
2026-01-13 10:42:19 +0100

    LayeredSection - possible to adjust height

--- public/src/js/Hawk/HawkLibrary/LayeredSection.Hawk.js
+++ public/src/js/Hawk/HawkLibrary/LayeredSection.Hawk.js
@@ -25,7 +25,21 @@ export default function LayeredSection(container, options) {
         nameAttribute: 'data-name',
         baseLayerName: 'base',
 
-        onAboveLayerShow: function (layeredSection, aboveLayer) {
+        settingSameHeightThreshold: 992,
+        settingSameHeight: false,
+
+        shouldApplySameHeight: function(layeredSection) {
+            return layeredSection.defaultShouldApplySameHeight();
+        },
+
+        onBaseLayerShowing: function(layeredSection, baseLayer) {
+
+        },
+
+        onAboveLayerShowing: function(layeredSection, aboveLayer) {
+
+        },
+        onAboveLayerShow: function(layeredSection, aboveLayer) {
         }
     };
 
@@ -33,29 +47,47 @@ export default function LayeredSection(container, options) {
 
     this.hideBaseLayer = function() {
         this.baseLayerInner.velocity({ opacity: 0 }, {
-           complete: function() {
-               that.baseLayer.css({ visibility: 'hidden' });
-           }
+            complete: function() {
+                that.baseLayer.css({ visibility: 'hidden' });
+            }
         });
 
         return this;
     }
 
-    this.showBaseLayer = function() {
+    this.showBaseLayer = function(launchCallback, adjustContainerHeight) {
         this.hideLayers();
 
         this.baseLayer.css({ visibility: 'visible' });
 
-        this.baseLayerInner.velocity({ opacity: 1 }, {
+        Hawk.writeDebugInfo("base Layer scrollHeight: ", this.baseLayerInner.get(0).scrollHeight);
 
-        });
+        if (!this.shouldApplySameHeight() && (typeof adjustContainerHeight == 'undefined' || adjustContainerHeight)) {
+            this.container.velocity({ height: this.baseLayerInner.get(0).scrollHeight + "px" }, {
+                complete: function () {
+                    that.baseLayerInner.velocity({opacity: 1}, {});
+                }
+            });
+        } else {
+            that.baseLayerInner.velocity({opacity: 1}, {});
+        }
+
+        if (typeof launchCallback == 'undefined' || launchCallback) {
+            this.options.onBaseLayerShowing(this, this.baseLayer);
+        }
 
         return this;
     }
 
     this.hideLayers = function(except) {
-        this.aboveLayers.not('[' + that.options.nameAttribute + '="' + except + '"]').velocity("fadeOut");
-    
+        var currentLayers = this.aboveLayers.not('[' + that.options.nameAttribute + '="' + except + '"]');
+
+        currentLayers.velocity({ opacity: 0 }, {
+            complete: function() {
+                currentLayers.css({ height: 0 });
+            }
+        });
+
         return this;
     }
 
@@ -66,21 +98,65 @@ export default function LayeredSection(container, options) {
 
         var currentLayer = that.aboveLayers.filter('[' + that.options.nameAttribute + '="' + name + '"]');
 
-        if (currentLayer.length > 0) {
-            currentLayer.velocity("fadeIn", {
-                complete: function() {
-                    // currentLayer.find('.' + that.options.aboveLayerInnerClass).css({
-                    //     opacity: 1
-                    // });
+        Hawk.writeDebugInfo("currentLayer scrollHeight: ", currentLayer.get(0).scrollHeight);
 
-                    that.options.onAboveLayerShow(that, currentLayer);
-                }
-            });
+        if (currentLayer.length > 0) {
+            if (!this.shouldApplySameHeight()) {
+                this.container.velocity({ height: currentLayer.find('.' + this.options.aboveLayerInnerClass).get(0).scrollHeight + "px" }, {
+                    complete: () => {
+                        currentLayer.css({ height: currentLayer.find('.' + this.options.aboveLayerInnerClass).get(0).scrollHeight + "px" }).velocity({ opacity: 1 }, {
+                            complete: function() {
+                                // currentLayer.find('.' + that.options.aboveLayerInnerClass).css({
+                                //     opacity: 1
+                                // });
+
+                                that.options.onAboveLayerShow(that, currentLayer);
+                            }
+                        });
+                    }
+                });
+            } else {
+                currentLayer.css({ height: '100%' }).velocity({ opacity: 1 }, {
+                    complete: function() {
+                        // currentLayer.find('.' + that.options.aboveLayerInnerClass).css({
+                        //     opacity: 1
+                        // });
+
+                        that.options.onAboveLayerShow(that, currentLayer);
+                    }
+                });
+            }
+
+            that.options.onAboveLayerShowing(that, currentLayer);
 
             return true;
         } else {
             return false;
         }
+    }
+
+    this.applySameHeightForAll = function() {
+        var maxHeight = 0;
+
+        this.baseLayer.add(this.aboveLayers).each(function() {
+            Hawk.writeDebugInfo("scrollHeight: " + $(this).get(0).scrollHeight);
+
+            if (maxHeight < $(this).get(0).scrollHeight) {
+                maxHeight = $(this).get(0).scrollHeight;
+            }
+        });
+
+        Hawk.writeDebugInfo("height to apply: " + maxHeight);
+
+        this.container.css({ 'min-height': maxHeight + "px" });
+    }
+
+    this.shouldApplySameHeight = function() {
+        return this.options.shouldApplySameHeight(this);
+    }
+
+    this.defaultShouldApplySameHeight = function() {
+        return this.options.settingSameHeight && this.options.settingSameHeightThreshold <= Hawk.w;
     }
 
     this.run = function() {
@@ -92,14 +168,18 @@ export default function LayeredSection(container, options) {
         this.aboveLayers = this.container.find('.' + this.options.aboveLayerClass);
 
         this.buttons.click(function() {
-           var name = $(this).attr(that.options.nameAttribute);
+            var name = $(this).attr(that.options.nameAttribute);
 
-           if (name !== that.options.baseLayerName) {
-               that.showLayer(name);
-           } else {
-               that.showBaseLayer();
-           }
+            if (name !== that.options.baseLayerName) {
+                that.showLayer(name);
+            } else {
+                that.showBaseLayer();
+            }
         });
+
+        if (this.shouldApplySameHeight()) {
+            this.applySameHeightForAll();
+        }
 
         return this;
     }
